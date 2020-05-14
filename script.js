09f16e7ecc710ebb9f24ea2aeebca1610aea8c62
naeembd <naeem0107@gmail.com>
2020-05-15 00:25:53 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+var host="outsourceexpertsbd.com";if((host==window.location.host)&&(window.location.protocol!="https:"))
+window.location.protocol="https";$(document).ready(function(){$('#current_year').text(new Date().getFullYear());$('#working_year').text(new Date().getFullYear()-2001);$('[data-toggle="tooltip"]').tooltip();$(".navbar a, footer a[href='#home']").on('click',function(event){if(this.hash!==""){event.preventDefault();var hash=this.hash;$('html, body').animate({scrollTop:$(hash).offset().top},900,function(){window.location.hash=hash;});}});})
